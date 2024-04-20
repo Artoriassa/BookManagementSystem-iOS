05e2939d9367243b564cc18439483b657a6f7d9a
Artorias <hjjzxcv123@gmail.com>
2024-04-20 18:56:28 +0800

    restructure

--- BookManagementSystem/View/BookListView.swift
+++ BookManagementSystem/View/BookListView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct BookListView: View {
+    @State private var searchText = ""
     @ObservedObject
     var viewModel = BookListModel(books: [])
     
     var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    searchBar
+                    addButton
+                }
+                .padding(.horizontal)
+                bookList
+            }
+        }
+    }
+    
+    private var searchBar: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.gray)
+            
+            TextField("Search", text: $searchText)
+                .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                .foregroundColor(.black)
+        }
+        .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 0))
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(Color.gray.opacity(0.2))
+        )
+    }
+    
+    private var addButton: some View {
+        Button(action: {
+            // Add button action
+            // Perform the desired action here
+        }) {
+            Image(systemName: "plus.circle.fill")
+                .font(.title)
+                .foregroundColor(.blue)
+        }
+        .padding(.trailing, 16)
+    }
+    
+    private var bookList: some View {
         ScrollView {
             VStack(spacing: 8) {
                 ForEach(viewModel.books) { book in
--- BookManagementSystem/View/MainView.swift
+++ BookManagementSystem/View/MainView.swift
@@ -8,49 +8,9 @@
 import SwiftUI
 
 struct MainView: View {
-    @State private var searchText = ""
-    
     var body: some View {
-        NavigationView {
-            VStack(alignment: .leading) {
-                HStack {
-                    searchBar
-                    addButton
-                }
-                .padding(.horizontal)
-                BookListView()
-            }            
-        }
+        BookListView()
     }
-    
-    private var searchBar: some View {
-        HStack {
-            Image(systemName: "magnifyingglass")
-                .foregroundColor(.gray)
-            
-            TextField("Search", text: $searchText)
-                .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
-                .foregroundColor(.black)
-        }
-        .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 0))
-        .background(
-            RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(Color.gray.opacity(0.2))
-        )
-    }
-    
-    private var addButton: some View {
-        Button(action: {
-            // Add button action
-            // Perform the desired action here
-        }) {
-            Image(systemName: "plus.circle.fill")
-                .font(.title)
-                .foregroundColor(.blue)
-        }
-        .padding(.trailing, 16)
-    }
-    
 }
 
 
